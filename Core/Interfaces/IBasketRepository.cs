c7b8039fe9c072d728c6cf045bac35844a310f57
geas0621 <geas0621@gmail.com>
2024-01-09 22:33:24 +0200

    End of Section 13

---
+++ Core/Interfaces/IBasketRepository.cs
@@ -0,0 +1,12 @@
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string BasketId);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(string BasketId);
+
+    }
+}
